8d364a12bd18df8d3ac60716eeb1590e006ab4ac
IsamarDavilaG <isamar.dvgz@gmail.com>
2017-03-13 11:03:10 -0500

    avance

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/**
+ * Created by gerson on 10/03/17.
+ */
+$(document).ready(init);
+
+var currentSection = null;
+
+function init()
+{
+	currentSection = $('#saludo');
+	$('#btn-saludo').click(onClickBtnSaludo);
+	$('#btn-nombres').click(onClickBtnNombre);
+    $('#btn-nombres').click(onClickBtnJuego);
+
+	TweenMax.from($('#saludo h1'), 1, {marginBottom:'0px', ease:Elastic.easeOut});
+}
+
+function onClickBtnSaludo() {
+	gotoSection('nombres');
+}
+
+function onClickBtnNombre() {
+	gotoSection('juego');
+}
+function onClickBtnJuego() {
+	gotoSection('juego');
+}
+function onClickBtnNombre() {
+	gotoSection('juego');
+}
+
+function gotoSection(_identificadorDeSeccion)
+{
+	currentSection.removeClass('visible');
+	var nextSection = $('#'+_identificadorDeSeccion);
+
+	nextSection.addClass('visible');
+
+	//TweenMax.from(nextSection, 1.5, {scale:0.2, opacity:0, ease:Elastic.easeOut});
+	currentSection = nextSection;
+}
