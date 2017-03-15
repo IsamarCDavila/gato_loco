4dad09b455f7da4c14d0215d1e93d87cf10e3f16
giuliana2043 <giulianna.2043@gmail.com>
2017-03-14 20:14:09 -0500

    alertas ok y arreglando hago por gano tbm placeholder

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ function drawHistorial(_datos) {
     var list = $('#list-games');
     for(var i in _datos){
         console.log(_datos[i].winner_player);
-        var html = '<li data-idgame="'+_datos[i].id+'"class="list-group-item">El jugador: '+_datos[i].winner_player +'le hago a '+_datos[i].loser_player+ '<button class="btn">Comentar</button></li>';
+        var html = '<li data-idgame="'+_datos[i].id+'"class="list-group-item">El jugador: '+_datos[i].winner_player +'le gano a '+_datos[i].loser_player+ '<button class="btn">Comentar</button></li>';
         list.append(html);
     }  
 }
